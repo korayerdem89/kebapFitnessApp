6d1687235af9c5e481b8091d68f7214738b29465
Koray Erdem <developerkoray@gmail.com>
2021-06-07 01:22:34 +0300

    "Member Sign-up page is OK"

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { Text, View, TextInput } from 'react-native'
-import styles from './input.style';
+import styles from './Input.style';
 
-function Input({label}){
+function Input({label, onChangeText, placeholder}){
     return(
    <View style={styles.container}>
        <Text style={styles.label}>{label}</Text>
        <View style={styles.input_container}>
-       <TextInput 
-           
-           />
-       </View>
-   
-
-     </View>
+       <TextInput  placeholder={placeholder} onChangeText={onChangeText}/>
+       </View></View>
     )
 }
+export default Input;
--- src/pages/MemberSign.js
+++ src/pages/MemberSign.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 function MemberSign(){
+
     return(
-<SafeAreaView>
-    <Text>Member Sign</Text>
-</SafeAreaView>
+
+<SafeAreaView style={styles.container}>
+    <View style={styles.input_container}>
+    <Input 
+    label="Üye Adı"
+    placeholder="İsminiz, giriniz..."
+    />
+      <Input 
+    label="Üye Soyadı"
+    placeholder="Soyisminiz, giriniz..."
+    />
+       <Input 
+    label="Üye Yaşı"
+    placeholder="Yaşınızı giriniz..."
+    />
+       <Input 
+    label="Üye E-posta"
+    placeholder="E-posta adresinizi giriniz..."
+    />
+       <Input 
+    label="Üye Şehir"
+    placeholder="Şehir giriniz"
+    />
+    </View>
+    <Button text="Kaydı Tamamla" onPress={null} />
+    </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      alignItems:'center',
+    
+    },
+    input_container:{
+        width:'90%',
+      
+    }
+});
 
 export default MemberSign;
